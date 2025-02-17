5b71d643c3fde222c01ae3120a68d393bde69e70
Sanjana <235uit036@gbu.ac.in>
2025-02-17 18:25:48 +0530

    Delete frontend directory

--- frontend/src/components/homepage/campus/CampusSection.jsx
+++
@@ -1,9 +0,0 @@
-import CampusSwiperComponent from "../../swiper/homepage/CampusSwiperComponent";
-
-export default function CampusSection() {
-  return (
-    <section className="h-[48rem] flex">
-      <CampusSwiperComponent />
-    </section>
-  );
-}
